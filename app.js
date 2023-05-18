c158483f1a9bd97de9b6fbf1465e2323efbb9270
Beafzzz <105944716+Beafzzz@users.noreply.github.com>
2023-05-18 19:24:45 +0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let tg = window.Telegram.Webapp;
 
 tg.expand();
 
-tg.MainButton.textColor="#FFFFFF"
+tg.MainButton.textColor="#FFFFFF";
 tg.MainButton.color="#FFFFFF";
 
 let btn = document.getElementById("btn");
@@ -10,5 +10,5 @@ let btn = document.getElementById("btn");
 btn.addEventListener("click", function(){
     tg.MainButton.setText("Сообщение отправлено!");
     tg.MainButton.show();
-    tg.sendData("Я отправиль")
+    tg.sendData("Я отправиль");
 });
